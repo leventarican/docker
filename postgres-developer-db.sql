c73d38a48c87a5eb949c046277858b9d3e129db9
leventarican <levent@protonmail.com>
2019-12-08 21:39:20 +0100

    docker and postgres

---
+++ postgres-developer-db.sql
@@ -0,0 +1,6 @@
+select * from programming_languages pl;
+
+insert into programming_languages (id, name) values (0, 'java');
+insert into programming_languages (id, name) values (1, 'python');
+
+commit;
