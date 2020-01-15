43fbb402cf50c73f6b04311e8087748bc054e347
leventarican <levent@protonmail.com>
2020-01-15 22:08:27 +0100

    flask os commands

--- flask-dockerfile/README.md
+++ flask-dockerfile/README.md
@@ -7,4 +7,8 @@
 curl -v 0.0.0.0:5000/ping
 
 curl localhost:5000/webhook -d '{"user":"foobar", "lang":"python"}' -H "Content-Type: application/json"
+
+curl localhost:5000/linux
+curl localhost:5000/git
+curl localhost:5000/python
 ```
--- flask-dockerfile/app.py
+++ flask-dockerfile/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from subprocess import check_output
 
 app = Flask(__name__)
 
@@ -19,6 +20,21 @@ def webhook():
         commit_user = data['user']
         print(f'webhook received. commit from {commit_user}.')
     return "webhook" + str(commit_user)
+
+@app.route("/linux")
+def linux():
+    stdout = check_output(['uname', '-a']).decode('utf-8')
+    return stdout
+
+@app.route("/git")
+def git():
+    stdout = check_output(['git', 'version']).decode('utf-8')
+    return stdout
+
+@app.route("/python")
+def python_version():
+    stdout = check_output(['python', '--version']).decode('utf-8')
+    return stdout
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0') # on localhost: bind ip
