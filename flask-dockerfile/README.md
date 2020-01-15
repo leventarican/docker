87f043c9fc1324f56580628a06693080e9e24f3b
leventarican <levent@protonmail.com>
2020-01-15 21:35:34 +0100

    json, post

--- flask-dockerfile/README.md
+++ flask-dockerfile/README.md
@@ -1,4 +1,10 @@
 # flask (python) docker container
 * build: `docker build -t leventarican/flask .`
 * run (bind host port): `docker run -p 5000:5000 leventarican/flask`
-* call: `curl -v 0.0.0.0:5000/ping`
+
+## example calls
+```
+curl -v 0.0.0.0:5000/ping
+
+curl localhost:5000/webhook -d '{"user":"foobar", "lang":"python"}' -H "Content-Type: application/json"
+```
--- flask-dockerfile/app.py
+++ flask-dockerfile/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +10,15 @@ def index():
 @app.route("/ping")
 def ping():
     return "pong"
+
+@app.route("/webhook", methods=['GET', 'POST'])
+def webhook():
+    commit_user = None
+    if request.method == 'POST':
+        data = request.get_json()
+        commit_user = data['user']
+        print(f'webhook received. commit from {commit_user}.')
+    return "webhook" + str(commit_user)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0') # on localhost: bind ip
